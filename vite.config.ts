8dbf9a883ff43da1f512de9434977dc122593315
jaycuh <70965524+jOaawd@users.noreply.github.com>
2025-09-01 22:59:59 +0000

    testin codsandbox

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,25 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  preview: {
-    host: "::",
-    port: 8080,
-    allowedHosts: ["macro-tracking-production.up.railway.app"],
-  },
-  plugins: [
-    react(),
-    
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    preview: {
+      host: "::",
+      port: 8080,
+      allowedHosts: [
+        "macro-tracking-production.up.railway.app",
+        "wjvm8n-8080.csb.app",
+      ],
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
+});
